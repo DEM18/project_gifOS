147ababdfa0f6b44522ff4c81dd356175160e71b
DEM18 <morenodanielaeliana@gmail.com>
2020-03-30 03:06:41 -0300

    Create MyGuifos section

--- javascript.js
+++ javascript.js
@@ -7,35 +7,41 @@ const EMPTY_STRING = "";
 const LIST_ID_TRENDING_GIFS = "list-trend-gifs";
 const LIST_ID_SUGGESTED_GIFS = "list-suggested-gifs";
 const SECTION_ID_SUGGESTED_GIFS = "suggestedGifSection";
+const SEARCH_BAR_ID = "search-bar";
 
 
 function getEndpoint(endpointURI, keyWord, resultsLimit, rating) { //returns string for endpoint
   return `${BASEURL}/${endpointURI}?api_key=${APIKEY}&q=${keyWord}&limit=${resultsLimit}&rating=${rating}`;
 }
 
-async function fetchGifs(endpointURI, keyWord, resultsLimit, rating) { //call to API for gifs
+async function fetchGifs(endpointURI, keyWord, resultsLimit, rating) { // fetch API for getting gifs
   let response = await fetch(getEndpoint(endpointURI, keyWord, resultsLimit, rating));
   let responseData = await response.json(); //devuelve archivo json
   let arrayGifsResults = responseData.data;
 
-  return arrayGifsResults; //return gifs
+  return arrayGifsResults;
 }
 
 function toggleDropdown() {
   document.getElementById("btn-dropdown").classList.toggle("show");
 }
 
-async function searchGif() { 
-  const empty = "";
-  let gifResultsContainer = document.getElementById("gif-results");
-  let sectionSuggestedGifs = document.getElementById(SECTION_ID_SUGGESTED_GIFS);
+// function enableSearchButton() {
+//   let buttonSearch = document.getElementById("btn-search");
 
-  console.log(sectionSuggestedGifs);
+//   if (SEARCH_BAR_ID !== "") {
+//     buttonSearch.disabled = false;
+//   }
+// }
+
+async function searchGif() { 
+  let gifResultsContainer = document.getElementById("gif-results");
   let inputText = document.getElementById("search-bar").value;
-  let gifResults = await fetchGifs(ENDPOINT_SERCH, inputText, 3, empty);
+  let sectionSuggestedGifs = document.getElementById(SECTION_ID_SUGGESTED_GIFS);
+  let gifResults = await fetchGifs(ENDPOINT_SERCH, inputText, 3, EMPTY_STRING);
   
   gifResultsContainer.innerHTML = "";
-  
+
   displayfetchGifsOnElementId( LIST_ID_TRENDING_GIFS, ENDPOINT_SERCH, inputText, EMPTY_STRING, EMPTY_STRING );
 
   gifResults.forEach(
@@ -45,7 +51,11 @@ async function searchGif() {
     );
     
     document.getElementById("btn-search").classList.toggle("show");
-    sectionSuggestedGifs.style.display = "none";
+    
+    if (gifResults.length ) {
+      sectionSuggestedGifs.style.display = "none";
+    } else null;
+
 
   }
  
@@ -63,7 +73,7 @@ async function displayfetchGifsOnElementId( elementId, endpointURI, keyWord, res
   let gifClass = document.getElementById(elementId);
   let gifResults = await fetchGifs( endpointURI, keyWord, resultsLimit, rating );
 
-  gifClass.innerHTML = "";
+  // gifClass.innerHTML = "";
 
   gifResults.forEach(
     gif =>
@@ -77,6 +87,28 @@ async function displayfetchGifsOnElementId( elementId, endpointURI, keyWord, res
     );
 }
 
+function createGif() {
+  let createGifModal = document.getElementById("createGifModal");
+  createGifModal.style.width = "860px"; 
+  createGifModal.style.height = "548px";
+  console.log(createGifModal);
+  createGifModal.innerHTML = `
+    <div class="first-step-title">
+      <span>Crear guifos</span>
+      <img class="close-btn" src="./assets/icons/close.svg">
+    </div>
+    <div class="getVideoCaption">
+      Video caption
+    </div>
+    <div class="navigation-buttons">
+    <button class='btn getCaptionImage'>
+      <img src="./assets/icons/camera.svg">
+    </button>
+      <button class='btn caption'>Capturar</button>
+    </div>
+  `
+}
+
 
 displayfetchGifsOnElementId( LIST_ID_SUGGESTED_GIFS, ENDPOINT_SERCH, "random", 4, EMPTY_STRING );
 displayfetchGifsOnElementId( LIST_ID_TRENDING_GIFS, ENDPOINT_TRENDING, EMPTY_STRING, EMPTY_STRING, "G" );
@@ -85,6 +117,6 @@ displayfetchGifsOnElementId( LIST_ID_TRENDING_GIFS, ENDPOINT_TRENDING, EMPTY_STR
 // changeTheme();
 /*-------------------------------*/
 //TODO
-//sacar empty, poner const global
+//sacar input search-bar como constante
 //arreglar nombres de variables
 //ordernar alfabeticamente
